a9236d72c0e3db9d6099dae77344fcea3d56b6d2
maciojunior <juniorlimafl@gmail.com>
2023-02-22 14:43:43 -0300

    Foi adicionado o 2º exercício- Calculo de área do circulo através do Raio

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
-﻿namespace Lista1
+﻿using System.Globalization;
+namespace Lista1
 {
     internal class Program
     {
         static void Main(string[] args)
-        {   /*Exercício 01
+        {
+            Exercicio1();
+            Exercicio2();
+        }
+
+        static void Exercicio1()
+        { 
+            /*Exercício 01
             Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
             mensagem explicativa, conforme exemplos.*/
 
@@ -13,6 +21,21 @@
             Console.Clear();
 
             Console.WriteLine(OneNumber+TwoNumber);
+        }
+
+        static void Exercicio2()
+        {
+            /* Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+            casas decimais conforme exemplos.
+            Fórmula da área: area = π . raio2
+            Considere o valor de π = 3.14159*/
+
+            double R, A, Pi = 3.14159;
+
+            R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            A = Pi * R * R;
+
+            Console.WriteLine("A=" + A.ToString("F4", CultureInfo.InvariantCulture));
         }
     }
 }
