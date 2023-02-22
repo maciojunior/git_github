a2e85415cafbccf75adfddb90f742a9199c91a98
maciojunior <juniorlimafl@gmail.com>
2023-02-22 11:38:54 -0300

    Exercicio1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Lista1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   /*Exercício 01
+            Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+            mensagem explicativa, conforme exemplos.*/
+
+            int OneNumber = int.Parse(Console.ReadLine());
+            Console.Clear();
+            int TwoNumber = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine(OneNumber+TwoNumber);
+        }
+    }
+}
